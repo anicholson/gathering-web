60ec58c8eb3354c2db0ae4a05f121bff82e73eb5
Andy Nicholson <andrew@anicholson.net>
2014-12-16 08:33:48 +1100

    Connect up to the Database on startup

--- config/initializers/01_lotus_repositories.rb
+++ config/initializers/01_lotus_repositories.rb
@@ -1,16 +1,22 @@
-
-require 'sqlite3'
+require 'pg'
 require 'lotus/model'
 require 'lotus/model/adapters/sql_adapter'
 
+
+DB_PATH = ENV['DATABASE_URL'] || "postgres://localhost:5432/gathering_#{Rails.env}"
 
 SQLAdapter = Lotus::Model::Adapters::SqlAdapter.new(
     Mappers::GatheringMapper,
     {
       adapter: 'sqlite',
-      database: ':memory:'
+      database: DB_PATH
     }
 )
+
+
+Gathering::REPOSITORIES.each do |repo|
+  repo.adapter = SQLAdapter
+end
 
 Mutex.new.synchronize do
   Mappers::GatheringMapper.load!
